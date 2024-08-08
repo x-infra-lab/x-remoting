a7d95b43f16bca4607e5cd1380a852c35c3d3ecf
joecqupt <joe469391363@gmail.com>
2024-08-09 01:05:20 +0800

    upate

--- .github/bin/check_format.sh
+++ .github/bin/check_format.sh
@@ -2,7 +2,7 @@
 
 # make sure git has no un commit files
 if [ -n "$(git status --untracked-files=no --porcelain)" ]; then
-   echo "Please commit your change before run this shell, un commit files:"
+   echo "Please check format for files:"
    git status --untracked-files=no --porcelain
    exit -1
 fi
