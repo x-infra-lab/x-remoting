64e9e63dd4b686d7715c4b0a34e02b9cd3aaa507
joecqupt <joe469391363@gmail.com>
2025-11-22 01:24:21 +0800

    update

--- x-remoting-core/src/test/java/io/github/xinfra/lab/remoting/impl/message/RemotingMessageFactoryTest.java
+++ x-remoting-core/src/test/java/io/github/xinfra/lab/remoting/impl/message/RemotingMessageFactoryTest.java
@@ -1,124 +1,84 @@
 package io.github.xinfra.lab.remoting.impl.message;
 
 import io.github.xinfra.lab.remoting.common.IDGenerator;
-import io.github.xinfra.lab.remoting.exception.ConnectionClosedException;
-import io.github.xinfra.lab.remoting.exception.SendMessageException;
-import io.github.xinfra.lab.remoting.exception.TimeoutException;
-import io.github.xinfra.lab.remoting.impl.RemotingProtocol;
-import io.github.xinfra.lab.remoting.impl.exception.RemotingServerException;
+import io.github.xinfra.lab.remoting.impl.RemotingProtocolIdentifier;
+import io.github.xinfra.lab.remoting.message.MessageType;
+import io.github.xinfra.lab.remoting.message.ResponseStatus;
 import io.github.xinfra.lab.remoting.serialization.SerializationType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 
 public class RemotingMessageFactoryTest {
 
-	@Test
-	public void testCreateRequestMessage() {
-		RemotingMessageFactory remotingMessageFactory = new RemotingMessageFactory();
+    @Test
+    public void testCreateRequestMessage() {
+        RemotingMessageFactory remotingMessageFactory = new RemotingMessageFactory();
 
-		RemotingRequestMessage requestMessage = remotingMessageFactory.createRequestMessage();
-		Assertions.assertNotNull(requestMessage);
-		Assertions.assertEquals(requestMessage.messageType(), RpcMessageType.request);
-		Assertions.assertArrayEquals(requestMessage.protocolIdentifier(), RemotingProtocol.PROTOCOL_CODE);
-		Assertions.assertEquals(requestMessage.serializationType(), SerializationType.HESSION);
-	}
+        RemotingRequestMessage requestMessage = remotingMessageFactory.createRequest(IDGenerator.nextRequestId(), SerializationType.Hession);
+        Assertions.assertNotNull(requestMessage);
+        Assertions.assertEquals(requestMessage.messageType(), MessageType.request);
+        Assertions.assertArrayEquals(requestMessage.protocolIdentifier().code(), RemotingProtocolIdentifier.PROTOCOL_CODE);
+        Assertions.assertEquals(requestMessage.serializationType(), SerializationType.Hession);
+        Assertions.assertNull(requestMessage.getPath());
+        Assertions.assertNull(requestMessage.headers());
+        Assertions.assertNull(requestMessage.body());
+    }
 
-	@Test
-	public void testCreateSendFailResponseMessage() throws UnknownHostException {
-		RemotingMessageFactory remotingMessageFactory = new RemotingMessageFactory();
-		InetSocketAddress remoteAddress = InetSocketAddress.createUnresolved("localhost", 8080);
+    @Test
+    public void testCreateSendFailResponseMessage() throws UnknownHostException {
+        RemotingMessageFactory remotingMessageFactory = new RemotingMessageFactory();
 
-		RemotingResponseMessage responseMessage = remotingMessageFactory.createSendFailedResponseMessage(
-				IDGenerator.nextRequestId(), new RuntimeException("testCreateSendFailResponseMessage"), remoteAddress);
-		Assertions.assertNotNull(responseMessage);
-		Assertions.assertEquals(responseMessage.getStatus(), ResponseStatus.CLIENT_SEND_ERROR.getCode());
-		Assertions.assertTrue(responseMessage.getCause() instanceof SendMessageException);
-		Assertions.assertEquals(responseMessage.messageType(), RpcMessageType.response);
-		Assertions.assertArrayEquals(responseMessage.protocolIdentifier(), RemotingProtocol.PROTOCOL_CODE);
-		Assertions.assertEquals(responseMessage.serializationType(), SerializationType.HESSION);
-	}
+        RemotingResponseMessage responseMessage = remotingMessageFactory.createResponse(
+                IDGenerator.nextRequestId(), SerializationType.Hession, ResponseStatus.SendFailed,
+                new UnknownHostException("testCreateSendFailResponseMessage"));
 
-	@Test
-	public void testCreateTimeoutResponseMessage() throws UnknownHostException {
-		RemotingMessageFactory remotingMessageFactory = new RemotingMessageFactory();
-		InetSocketAddress remoteAddress = InetSocketAddress.createUnresolved("localhost", 8080);
+        Assertions.assertNotNull(responseMessage);
+        Assertions.assertEquals(responseMessage.messageType(), MessageType.response);
+        Assertions.assertEquals(responseMessage.responseStatus(), ResponseStatus.SendFailed);
+        Assertions.assertEquals(responseMessage.serializationType(), SerializationType.Hession);
+        Assertions.assertArrayEquals(responseMessage.protocolIdentifier().code(), RemotingProtocolIdentifier.PROTOCOL_CODE);
 
-		RemotingResponseMessage responseMessage = remotingMessageFactory
-			.createTimeoutResponseMessage(IDGenerator.nextRequestId(), remoteAddress);
-		Assertions.assertNotNull(responseMessage);
-		Assertions.assertEquals(responseMessage.getStatus(), ResponseStatus.TIMEOUT.getCode());
-		Assertions.assertTrue(responseMessage.getCause() instanceof TimeoutException);
-		Assertions.assertEquals(responseMessage.messageType(), RpcMessageType.response);
-		Assertions.assertArrayEquals(responseMessage.protocolIdentifier(), RemotingProtocol.PROTOCOL_CODE);
-	}
+        Assertions.assertNull(responseMessage.headers());
+        Assertions.assertTrue(responseMessage.body().getBodyValue() instanceof UnknownHostException);
+    }
 
-	@Test
-	public void testCreateConnectionClosedMessage() throws UnknownHostException {
-		RemotingMessageFactory remotingMessageFactory = new RemotingMessageFactory();
-		InetSocketAddress remoteAddress = InetSocketAddress.createUnresolved("localhost", 8080);
+    @Test
+    public void testCreateTimeoutResponseMessage() {
+        RemotingMessageFactory remotingMessageFactory = new RemotingMessageFactory();
 
-		RemotingResponseMessage responseMessage = remotingMessageFactory
-			.createConnectionClosedMessage(IDGenerator.nextRequestId(), remoteAddress);
-		Assertions.assertNotNull(responseMessage);
-		Assertions.assertEquals(responseMessage.getStatus(), ResponseStatus.CONNECTION_CLOSED.getCode());
-		Assertions.assertTrue(responseMessage.getCause() instanceof ConnectionClosedException);
-		Assertions.assertEquals(responseMessage.messageType(), RpcMessageType.response);
-		Assertions.assertArrayEquals(responseMessage.protocolIdentifier(), RemotingProtocol.PROTOCOL_CODE);
-	}
+        RemotingResponseMessage responseMessage = remotingMessageFactory
+                .createResponse(IDGenerator.nextRequestId(), SerializationType.Hession, ResponseStatus.Timeout);
+        Assertions.assertNotNull(responseMessage);
+        Assertions.assertEquals(responseMessage.responseStatus(), ResponseStatus.Timeout);
+        Assertions.assertEquals(responseMessage.body(), null);
 
-	@Test
-	public void testCreateHeartbeatRequestMessage() {
-		RemotingMessageFactory remotingMessageFactory = new RemotingMessageFactory();
+    }
 
-		RemotingRequestMessage requestMessage = remotingMessageFactory.createHeartbeatRequestMessage();
-		Assertions.assertNotNull(requestMessage);
-		Assertions.assertEquals(requestMessage.messageType(), RpcMessageType.heartbeatRequest);
-		Assertions.assertArrayEquals(requestMessage.protocolIdentifier(), RemotingProtocol.PROTOCOL_CODE);
-	}
+    @Test
+    public void testCreateConnectionClosedMessage() throws UnknownHostException {
+        RemotingMessageFactory remotingMessageFactory = new RemotingMessageFactory();
 
-	@Test
-	public void testCreateResponse1() {
-		String responseContent = "this is response content";
-		RemotingMessageFactory remotingMessageFactory = new RemotingMessageFactory();
-		RemotingResponseMessage response = remotingMessageFactory.createResponse(IDGenerator.nextRequestId(),
-				responseContent);
+        RemotingResponseMessage responseMessage = remotingMessageFactory
+                .createResponse(IDGenerator.nextRequestId(), SerializationType.Hession, ResponseStatus.ConnectionClosed);
+        Assertions.assertNotNull(responseMessage);
+        Assertions.assertEquals(responseMessage.responseStatus(), ResponseStatus.ConnectionClosed);
+        Assertions.assertEquals(responseMessage.body(), null);
+    }
 
-		Assertions.assertNotNull(response);
-		Assertions.assertEquals(response.messageType(), RpcMessageType.response);
-		Assertions.assertArrayEquals(response.protocolIdentifier(), RemotingProtocol.PROTOCOL_CODE);
-		Assertions.assertEquals(response.getStatus(), ResponseStatus.SUCCESS.getCode());
-		Assertions.assertEquals(response.getContent(), responseContent);
-	}
+    @Test
+    public void testCreateHeartbeatRequestMessage() {
+        RemotingMessageFactory remotingMessageFactory = new RemotingMessageFactory();
 
-	@Test
-	public void testCreateExceptionResponse1() {
-		String errorMsg = "testCreateExceptionResponse1";
-		RemotingMessageFactory remotingMessageFactory = new RemotingMessageFactory();
-		RemotingResponseMessage response = remotingMessageFactory.createExceptionResponse(IDGenerator.nextRequestId(),
-				errorMsg);
-
-		Assertions.assertNotNull(response);
-		Assertions.assertEquals(response.messageType(), RpcMessageType.response);
-		Assertions.assertArrayEquals(response.protocolIdentifier(), RemotingProtocol.PROTOCOL_CODE);
-		Assertions.assertEquals(response.getStatus(), ResponseStatus.SERVER_EXCEPTION.getCode());
-		Assertions.assertInstanceOf(RemotingServerException.class, response.getContent());
-	}
-
-	@Test
-	public void testCreateExceptionResponse2() {
-		String errorMsg = "testCreateExceptionResponse2";
-		RemotingMessageFactory remotingMessageFactory = new RemotingMessageFactory();
-		RemotingResponseMessage response = remotingMessageFactory.createExceptionResponse(IDGenerator.nextRequestId(),
-				new RuntimeException("testCreateExceptionResponse2"), errorMsg);
-
-		Assertions.assertNotNull(response);
-		Assertions.assertEquals(response.messageType(), RpcMessageType.response);
-		Assertions.assertArrayEquals(response.protocolIdentifier(), RemotingProtocol.PROTOCOL_CODE);
-		Assertions.assertEquals(response.getStatus(), ResponseStatus.SERVER_EXCEPTION.getCode());
-		Assertions.assertInstanceOf(RemotingServerException.class, response.getContent());
-	}
+        RemotingRequestMessage requestMessage = remotingMessageFactory.createHeartbeatRequest(IDGenerator.nextRequestId(),
+                SerializationType.Hession);
+        Assertions.assertNotNull(requestMessage);
+        Assertions.assertEquals(requestMessage.messageType(), MessageType.heartbeatRequest);
+        Assertions.assertArrayEquals(requestMessage.protocolIdentifier().code(), RemotingProtocolIdentifier.PROTOCOL_CODE);
+        Assertions.assertNull(requestMessage.getPath());
+        Assertions.assertNull(requestMessage.headers());
+        Assertions.assertNull(requestMessage.body());
+    }
 
 }
